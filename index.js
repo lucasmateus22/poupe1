a3cf8e7e52cb99af62e5e7b47d5c9501e3403fcf
lucasmateus22 <104775719+lucasmateus22@users.noreply.github.com>
2022-10-06 21:32:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+window.onscroll=function(){scrollFunction()};
+
+function scrollFunction(){
+    if(document.body.scroll > 100 || document.documentElement.scrollTop > 50){
+        document.getElementById("btnScroll").style.display = "block";
+    }else{
+        document.getElementById("btnScroll").style.display = "none";
+    }
+}
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0; 
+};
+
